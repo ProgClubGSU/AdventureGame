41095f7ec4b86214e699222e3b7ada11143e59b0
Andrew Huang <shuang17@gsu.edu>
2021-03-30 18:16:35 +0000

    Location wiring

--- AdventureData.java
+++ AdventureData.java
@@ -6,6 +6,10 @@ public class AdventureData {
 		Location langdaleHall = new Location("Langdale Hall", "You are at Langdale Hall, also known as the Language Building.");
 		Location admissionsOffice = new Location("Admissions Office", "");
 
+    Location cafeteria = new Location("Cafeteria", "You see a bunch of tables, some places to purchase breakfast, and a set of stairs that go off to where the Disciplinary Board Member is standing angrily.");
+    Location cookieStall = new Location("Cookie Stall", "You see many boxes of all sorts of baked goods. Glenda sees you looking at her products and she smiles proudly.");
+    Location pantherStatue = new Location("The Panther Statue", "The panther has profanity scrawled all over it in bright red marker.");
+
 		// Characters
 		Character SteveSminkle = new Character("Steve Sminkle", admissionsOffice);
 		Character Xavier = new Character("Xavier", langdaleHall);
@@ -21,6 +25,19 @@ public class AdventureData {
     studentCenter.addItem(securityTape);
     studentCenter.addItem(redMarker);
     studentCenter.addItem(receiptOfTotalSale);
+
+    studentCenter.addNeighbor("further west", admissionsOffice);
+    admissionsOffice.addNeighbor("further east", studentCenter);
+    
+    studentCenter.addNeighbor("west", cookieStall);
+    cookieStall.addNeighbor("back", studentCenter);
+    studentCenter.addNeighbor("north", cafeteria);
+    cafeteria.addNeighbor("outside", studentCenter);
+    studentCenter.addNeighbor("south", pantherStatue);
+    pantherStatue.addNeighbor("back", studentCenter);
+
+    langdaleHall.addNeighbor("further east", admissionsOffice);
+    admissionsOffice.addNeighbor("further west", langdaleHall);
 
     return new Character(mainCharacterName, studentCenter);
   }
