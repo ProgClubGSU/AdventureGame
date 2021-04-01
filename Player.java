6ca825a9f2a8aaa9ef5cb63f5563b1c39b46eac4
Shardul Vaidya <31039336+5herlocked@users.noreply.github.com>
2021-03-31 22:36:43 -0400

    Delete Player.java

--- Player.java
+++
@@ -1,8 +0,0 @@
-public class Player extends Character {
-
-    public Player(String name, Location location) {
-        super(name, location);
-        //TODO Auto-generated constructor stub
-    }
-    
-}
