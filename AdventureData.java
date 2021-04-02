e19f7a72471bc95a2c7429ea6fb9d204587c0e22
Adithya Venkatesh <>
2021-04-02 06:36:30 +0000

    Glenda script fixed

--- AdventureData.java
+++ AdventureData.java
@@ -166,7 +166,8 @@ public class AdventureData {
 
 										//If User has the Red Marker Xavier displays this text:
 										// new SimpleEntry<>(
-										// 	"Woah… you got this from Glenda? This looks just like the kind of marker used on the statue! But it isn’t enough by itself… Why would she do something like that?",null,new ArrayList<>(),List.of(redMarker)
+										// 	"Woah… you got this from Glenda? This looks just like the kind of marker used on the statue! But it isn’t enough by itself… Why would she do something like that?"
+										// 	// null,new ArrayList<>(),new ArrayList<>(List.of(redMarker))
 										// )
 
 										// //If User has the Receipt of Total Sale Xavier displays this text:
@@ -204,33 +205,39 @@ public class AdventureData {
 							"Glenda brightens up and proudly proclaims, \"Business has been great! It’s really been booming since this morning. You know, cause like, the panther and all. People are really fascinated.\"", 
 							new ArrayList<>(
 								List.of(
-									new DialogNode<>(
-										"Glenda brightens up and proudly proclaims, \"Business has been great! It’s really been booming since this morning. You know, cause like, the panther and all. People are really fascinated.\""
-										// new ArrayList<>(
-										// 	List.of(
-										// 		new SimpleEntry<>(
-										// 			"I’ve got some free time if you need any help with anything.", 
-										// 			new DialogNode(
-										// 				"\"That would be great! I’ve been swamped with all the business, but I want to be MORE swamped! Could you make some flyers for me? I’d really appreciate it.\"")
-										// 				)
-										// 				// new SimpleEntry<>(
-										// 				// 	"How would you like to be nominated for the annual uh… 9-year-olds of entrepreneurial excellence award?",
-										// 				// 	new DialogNode(
-										// 				// 		"\"Oh boy would I!!\" You sneakily tell Glenda you’ll need proof of all the incredible sales she’s been making. She happily reaches into her little makeshift register and pulls out the Receipt of Total Sale.")
-										// 				// )
-										// 			)
-										// 		)
-											)
+									new SimpleEntry<>(
+										"I’ve got some free time if you need any help with anything.", 
+										new DialogNode(
+											"\"That would be great! I’ve been swamped with all the business, but I want to be MORE swamped! Could you make some flyers for me? I’d really appreciate it.\"")
+									),
+									new SimpleEntry<>(
+										"How would you like to be nominated for the annual uh… 9-year-olds of entrepreneurial excellence award?",
+											new DialogNode(
+											"\"Oh boy would I!!\" You sneakily tell Glenda you’ll need proof of all the incredible sales she’s been making. She happily reaches into her little makeshift register and pulls out the Receipt of Total Sale.")
 									)
+								)
 							)
-
 							)
+						)
 						)
 					)
 				)
-			)
-		);
-			
+			);
+
+			// new ArrayList<>(
+			// 								List.of(
+			// 									new SimpleEntry<>(
+			// 										"I’ve got some free time if you need any help with anything.", 
+			// 										new DialogNode(
+			// 											"\"That would be great! I’ve been swamped with all the business, but I want to be MORE swamped! Could you make some flyers for me? I’d really appreciate it.\"")
+			// 											)
+			// 											// new SimpleEntry<>(
+			// 											// 	"How would you like to be nominated for the annual uh… 9-year-olds of entrepreneurial excellence award?",
+			// 											// 	new DialogNode(
+			// 											// 		"\"Oh boy would I!!\" You sneakily tell Glenda you’ll need proof of all the incredible sales she’s been making. She happily reaches into her little makeshift register and pulls out the Receipt of Total Sale.")
+			// 											// )
+			// 										)
+			// 							)
 				
 
 		// Steve Sminkle Script:
